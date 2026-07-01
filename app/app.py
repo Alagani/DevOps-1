c81ae283673653505df25cd569f55c729e51c8c0
Alagani Jagadeesh <a.jagadeesh@exafluence.com>
2026-07-01 20:02:25 +0530

    update code

--- app/app.py
+++ app/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+data = []
+
 @app.route("/")
-def hello():
-    return "Hello Gayatri!"
+def home():
+    return "API is running"
+
+@app.route("/data")
+def get_data():
+    return jsonify(data)
+
+@app.route("/data", methods=["POST"])
+def add_data():
+    item = request.get_json()
+    data.append(item)
+    return jsonify(item), 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
